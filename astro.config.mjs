e6d9d437ed8858b53af603a1dfbd51de84ada641
RamiroG8k <ramiro.g8k@gmail.com>
2025-02-23 10:38:58 -0600

    Update initial config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
+import mdx from '@astrojs/mdx';
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    experimental: {
+        svg: true
+    },
+    integrations: [mdx(), sitemap()],
+    output: 'static',
+    site: 'http://localhost:4321'
+});
